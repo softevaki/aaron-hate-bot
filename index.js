f69321e1f3803e91a8b17abf927c0db82f2bea91
softevaki <evinikitopoulou06@gmail.com>
2026-02-17 12:11:15 +0200

    add express for render

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot is alive!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Web server running on port ${PORT}`);
+});
+
 require('dotenv').config();
 
 const { 
